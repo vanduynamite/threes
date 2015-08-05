021b568412e9f96de3f3663905849cd1a9890f11
ryanglambert <ryan.g.lambert@gmail.com>
2015-08-04 17:47:12 -0700

    adding test to check sliding doesn't replace when it shouldn't

---
+++ tests/squish_replace_correctly_tests.py
@@ -0,0 +1,38 @@
+from nose.tools import *
+import random
+import threes
+
+def setup():
+    global board
+    board = threes.Board()
+    board.choose_placement_for_queued_cards = lambda : [0]
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_slide_left():
+    board.replacement_stack = [1]
+    board.grid = [
+            [24,2,6,2],
+            [12,2,0,0],
+            [1,6,0,0],
+            [0,2,0,0]
+            ]
+
+    slid_board = [
+            [24,2,6,2],
+            [12,2,0,0],
+            [1,6,0,0],
+            [2,0,0,0]
+            ]
+
+
+    board.slide_left()
+    assert_equal(board.grid, slid_board)
+
+
+
+
+setup()
+test_slide_left()
+teardown()
